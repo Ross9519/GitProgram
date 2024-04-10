9a1f8fca7eb68fa711eef8dd1ec09b828ee5f709
Ross9519 <gianross05@yahoo.com>
2024-04-10 15:21:31 +0200

    new method ShowContacts() and modified AddContact()

--- GitProgram/Contact.cs
+++ GitProgram/Contact.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GitProgram
+﻿namespace GitProgram
 {
     internal class Contact
     {
@@ -15,6 +9,11 @@ namespace GitProgram
         {
             Name = name;
             Number = number;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Number: {Number}";
         }
     }
 }
--- GitProgram/Program.cs
+++ GitProgram/Program.cs
@@ -3,16 +3,35 @@
 List<Contact> contacts = [];
 
 AddContact(contacts);
+ShowContacts(contacts);
 
 static void AddContact(List<Contact> contacts)
 {
+    string name;
+    string number;
     Console.WriteLine("New Contact:");
-    Console.Write("Name: ");
-    string name = Console.ReadLine();
-    Console.Write("Number: ");
-    string number = Console.ReadLine();
+    do
+    {
+        Console.Write("Name: ");
+        name = Console.ReadLine();
+    } while (string.IsNullOrEmpty(name));
+    do
+    {
+        Console.Write("Number: ");
+        number = Console.ReadLine();
+    } while (string.IsNullOrWhiteSpace(number));
 
     Contact contact = new(name, number);
     contacts.Add(contact);
 
+}
+
+static void ShowContacts(List<Contact> contacts)
+{
+    Console.WriteLine("Here all the contacts in contact list");
+
+    foreach (var item in contacts)
+    {
+        Console.WriteLine(item);
+    }
 }
