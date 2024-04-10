ba2090e1932b98b5b4731e28079be05201bcb3f3
Ross9519 <gianross05@yahoo.com>
2024-04-10 16:09:00 +0200

    Reset (avevo capito male come strutturare i push)

--- GitProgram/Program.cs
+++ GitProgram/Program.cs
@@ -45,76 +45,20 @@ static void SelectMethod(string cmd, List<Contact> contacts)
 
 static void AddContact(List<Contact> contacts)
 {
-    string name;
-    string number;
-    Console.WriteLine("New Contact:");
-    do
-    {
-        Console.Write("Name: ");
-        name = Console.ReadLine();
-    } while (string.IsNullOrEmpty(name));
-    do
-    {
-        Console.Write("Number: ");
-        number = Console.ReadLine();
-    } while (string.IsNullOrWhiteSpace(number));
-
-    Contact contact = new(name, number);
-    contacts.Add(contact);
-
+    
 }
 
 static void ShowContacts(List<Contact> contacts)
 {
-    Console.WriteLine("Here all the contacts in contact list");
-
-    foreach (var item in contacts)
-    {
-        Console.WriteLine(item);
-    }
+    
 }
 
 static void FindContact(List<Contact> contacts)
 {
-    Console.WriteLine("Let's find a contact in contact list:");
-    string name;
-    do
-    {
-        Console.Write("Name: ");
-        name = Console.ReadLine();
-    } while (string.IsNullOrEmpty(name));
-
-    Contact foundContact = contacts.Find(contact => contact.Name.ToLower().Equals(name.ToLower()));
-
-    if (foundContact != null)
-    {
-        Console.WriteLine("Contact Found:");
-        Console.WriteLine(foundContact);
-    }
-    else
-    {
-        Console.WriteLine("Contact Not Found");
-    }
+    
 }
 
 static void RemoveContact(List<Contact> contacts)
 {
-    Console.WriteLine("Let's remove a contact in contact list:");
-    string name;
-    do
-    {
-        Console.Write("Name: ");
-        name = Console.ReadLine();
-    } while (string.IsNullOrEmpty(name));
-
-    Contact contact = contacts.Find(contact => contact.Name.ToLower().Equals(name.ToLower()));
-    if (contact != null)
-    {
-        contacts.Remove(contact);
-        Console.WriteLine("Contact Removed");
-    }
-    else
-    {
-        Console.WriteLine("Contact Not Found");
-    }
+    
 }
