87e29da233c3fb906b1ce806f555452bcf85d9a0
Ross9519 <gianross05@yahoo.com>
2024-04-10 15:41:32 +0200

    added method RemoveContact() and modified FindContact()

--- GitProgram/Program.cs
+++ GitProgram/Program.cs
@@ -4,13 +4,14 @@ List<Contact> contacts =
 [
     new("Gianluca", "5284562945"), 
     new("Pippo", "3937505154"),
-    new("Pluto", "3047592385"),
-    new("Pippo", "5849640234")
+    new("Pluto", "3047592385")
 ];
 
-AddContact(contacts);
+//AddContact(contacts);
+//ShowContacts(contacts);
+//FindContact(contacts);
+RemoveContact(contacts);
 ShowContacts(contacts);
-FindContact(contacts);
 
 
 
@@ -55,11 +56,37 @@ static void FindContact(List<Contact> contacts)
         name = Console.ReadLine();
     } while (string.IsNullOrEmpty(name));
 
-    List<Contact> foundContacts = contacts.FindAll(contact => contact.Name.ToLower().Equals(name.ToLower()));
-    
-    Console.WriteLine("Contacts Found:");
-    foreach (var item in foundContacts)
+    Contact foundContact = contacts.Find(contact => contact.Name.ToLower().Equals(name.ToLower()));
+
+    if (foundContact != null)
     {
-        Console.WriteLine(item);
+        Console.WriteLine("Contact Found:");
+        Console.WriteLine(foundContact);
+    }
+    else
+    {
+        Console.WriteLine("Contact Not Found");
+    }
+}
+
+static void RemoveContact(List<Contact> contacts)
+{
+    Console.WriteLine("Let's remove a contact in contact list:");
+    string name;
+    do
+    {
+        Console.Write("Name: ");
+        name = Console.ReadLine();
+    } while (string.IsNullOrEmpty(name));
+
+    Contact contact = contacts.Find(contact => contact.Name.ToLower().Equals(name.ToLower()));
+    if (contact != null)
+    {
+        contacts.Remove(contact);
+        Console.WriteLine("Contact Removed");
+    }
+    else
+    {
+        Console.WriteLine("Contact Not Found");
     }
 }
