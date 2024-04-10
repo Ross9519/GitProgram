9fcc993cc97020c1f8e39ba9c58565b11f6d040c
Ross9519 <gianross05@yahoo.com>
2024-04-10 15:32:38 +0200

    added method FindContact()

--- GitProgram/Program.cs
+++ GitProgram/Program.cs
@@ -1,9 +1,18 @@
 ﻿using GitProgram;
 
-List<Contact> contacts = [];
+List<Contact> contacts = 
+[
+    new("Gianluca", "5284562945"), 
+    new("Pippo", "3937505154"),
+    new("Pluto", "3047592385"),
+    new("Pippo", "5849640234")
+];
 
 AddContact(contacts);
 ShowContacts(contacts);
+FindContact(contacts);
+
+
 
 static void AddContact(List<Contact> contacts)
 {
@@ -31,6 +40,25 @@ static void ShowContacts(List<Contact> contacts)
     Console.WriteLine("Here all the contacts in contact list");
 
     foreach (var item in contacts)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+static void FindContact(List<Contact> contacts)
+{
+    Console.WriteLine("Let's find a contact in contact list:");
+    string name;
+    do
+    {
+        Console.Write("Name: ");
+        name = Console.ReadLine();
+    } while (string.IsNullOrEmpty(name));
+
+    List<Contact> foundContacts = contacts.FindAll(contact => contact.Name.ToLower().Equals(name.ToLower()));
+    
+    Console.WriteLine("Contacts Found:");
+    foreach (var item in foundContacts)
     {
         Console.WriteLine(item);
     }
