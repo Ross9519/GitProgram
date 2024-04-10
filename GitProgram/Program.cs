1af9bff35883e88bfdb993a2462a984aea352309
Ross9519 <gianross05@yahoo.com>
2024-04-10 15:14:45 +0200

    added method addContact()

--- GitProgram/Program.cs
+++ GitProgram/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using GitProgram;
+
+List<Contact> contacts = [];
+
+AddContact(contacts);
+
+static void AddContact(List<Contact> contacts)
+{
+    Console.WriteLine("New Contact:");
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Number: ");
+    string number = Console.ReadLine();
+
+    Contact contact = new(name, number);
+    contacts.Add(contact);
+
+}
