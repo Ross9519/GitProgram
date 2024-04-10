5a5c9b6dfc213f02e7bbb4ef50cea53fe6760d2d
Ross9519 <gianross05@yahoo.com>
2024-04-10 16:17:43 +0200

    added addcontact method

--- GitProgram/Program.cs
+++ GitProgram/Program.cs
@@ -45,7 +45,23 @@ static void SelectMethod(string cmd, List<Contact> contacts)
 
 static void AddContact(List<Contact> contacts)
 {
-    
+    string name;
+    string number;
+    Console.WriteLine("New Contact:");
+    do
+    {
+        Console.Write("Name: ");
+        name = Console.ReadLine();
+    } while (string.IsNullOrEmpty(name));
+    do
+    {
+        Console.Write("Number: ");
+        number = Console.ReadLine();
+    } while (string.IsNullOrWhiteSpace(number));
+
+    Contact contact = new(name, number);
+    contacts.Add(contact);
+
 }
 
 static void ShowContacts(List<Contact> contacts)
